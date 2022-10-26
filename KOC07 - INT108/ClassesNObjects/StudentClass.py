f4c90257bccf203f81931ca9256539ec13d06fdf
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-10-26 08:52:53 +0530

    Create StudentClass.py

---
+++ KOC07 - INT108/ClassesNObjects/StudentClass.py
@@ -0,0 +1,45 @@
+class Student:
+    def __init__(self, rollNo, name, marks):
+        self.rollNo = rollNo
+        self.name = name
+        self.marks = marks
+
+    def print(self):
+        print(self.rollNo , self.name, self.marks)
+
+
+#how to create object
+student1 = Student(1, "Ayushi", 25)
+student1.print()
+
+#how to create object
+student2 = Student(2, "Ashima", 30)
+student2.print()
+
+
+
+
+
+#errorenous code
+#a lot of variables
+# roll1 = 1
+# name1 = "Ayushi"
+# marks1 = 25
+#
+# roll2 = 2
+# name2 = "Ashima"
+# marks2 = 30
+#
+# roll3 = 3
+# name3 = "Vivek"
+# marks3 = 20
+#
+# roll4 = 4
+# name4 = "Vidushi"
+# marks4 = 25
+#
+# roll5 = 5
+# name5 = "Radhika"
+# marks5 = 27
+
+
