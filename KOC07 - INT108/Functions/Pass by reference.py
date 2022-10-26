a78229762d6597891c84b06d7bddc71ed3060c1c
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-10-26 08:19:46 +0530

    Create Pass by reference.py

---
+++ KOC07 - INT108/Functions/Pass by reference.py
@@ -0,0 +1,17 @@
+def demo(x):
+    x = [100,200,300,400,500]
+    x[0]=1000;
+    print(x)
+
+l=[10,20,30,40,50]
+demo(l)
+print(l)
+
+
+def demo(x):
+    x[0]=1000;
+    print(x)
+
+l=[10,20,30,40,50]
+demo(l)
+print(l)
