efcd259f91ffbde6050f1c46d1df9e09281fa70e
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-07 08:23:38 +0530

    Create Multiple Inheritance.py

---
+++ KOC07 - INT108/ClassesNObjects/Inheritance/Multiple Inheritance.py
@@ -0,0 +1,26 @@
+"""
+Mulitple Inheritance
+
+A Base class          B Base class  
+          |            | 
+          C Derived class
+
+"""
+
+class A:
+    def __init__(self):
+        print("A base class constructor called")
+
+class B:
+    def __init__(self):
+        print("B base class constructor called")
+
+class C(A, B):
+    def __init__(self):
+        A.__init__(self)
+        B.__init__(self)
+        print("C derived class constructor called")
+
+print("Start of program")
+c= C()
+print("End of program")
