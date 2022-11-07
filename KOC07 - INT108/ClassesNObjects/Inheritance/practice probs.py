9da3a5bf4fca1ad703b5fcfa28ae006ae48a1acd
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-07 09:08:40 +0530

    Create practice probs.py

---
+++ KOC07 - INT108/ClassesNObjects/Inheritance/practice probs.py
@@ -0,0 +1,12 @@
+PRACTICE:
+
+1. Create a class Student with two instance attributes student_name and student_marks. Create two member functions
+a. print - to print the information of student (name and marks)
+b. passOrFail - based on the marks, check whether student is pass or fail (pass if marks are greater than 33)
+
+2. Create two derived class A and B inheriting from a base class C, then A class itself is base class for class D.
+
+3. Create a class named Shape with function that prints "This is a shape"
+Create another class named Polygon inheriting the Shape class with function that prints "Polygon is a shape"
+Create two other classes Rectangle and Triangle inheriting from Polygon class having functions that say
+"Rectangle is a polygon" and "Triangle is a polygon"
