cef6b94674c70dde71fc828c71eb5378a2d47d96
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-07 08:40:25 +0530

    Create Hierarchal Inheritance.py

---
+++ KOC07 - INT108/ClassesNObjects/Inheritance/Hierarchal Inheritance.py
@@ -0,0 +1,27 @@
+"""
+Hierarichal Inheritance
+
+                            ____   __    A base class  ____
+                          |                    |               |
+                 B derived class      C derived class    D derived class
+"""
+
+class Parent:
+    def func1(self):
+        print("Base class function")
+
+class Child1(Parent):
+    def func2(self):
+        self.func1()
+        print("Child class 1 function")
+
+class Child2(Parent) :
+    def func3(self):
+        self.func1()
+        print("Child class 2 function")
+
+c1=Child1()
+c1.func2()
+c2=Child2()
+c2.func3()
+
