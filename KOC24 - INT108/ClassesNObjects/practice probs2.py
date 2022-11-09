db7b24e239db2039cd3b7a0d1b87d448bc201e1b
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-09 09:49:52 +0530

    Update practice probs2.py

--- KOC24 - INT108/ClassesNObjects/practice probs2.py
+++ KOC24 - INT108/ClassesNObjects/practice probs2.py
@@ -6,3 +6,24 @@ it should have 3 functions-
 3. to find the average(it should take list as argument)
 Create object of class and perform these 3 functions
 """
+class Calculate:
+    def __init__(self, x,y):
+        self.first = x
+        self.second = y
+
+    def modulus(self):
+        print(self.first % self.second)
+
+    def power(self, x, y):
+        print(x**y)
+
+    def average(self, l):
+        s = sum(l)
+        print(s/len(l))
+
+x=int(input("input x:"))
+y=int(input("input y:"))
+o1= Calculate(x,y)
+o1.modulus();
+o1.average([1,2,3,4,5])
+o1.power(2,4);
