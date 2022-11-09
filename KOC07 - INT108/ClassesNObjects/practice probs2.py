aa69b71d5126a40a4cef669a43a882ff80056696
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-09 08:35:08 +0530

    Create practice probs2.py

---
+++ KOC07 - INT108/ClassesNObjects/practice probs2.py
@@ -0,0 +1,12 @@
+"""
+
+Create a class Calculate
+it should have 3 functions-
+1. to find the modulus of two numbers (two numbers should be instance variables)
+2. to find the power (x**y --> take x and y as argument)
+3. to find the average(it should take list as argument)
+
+Create object of class and perform these 3 functions
+
+
+"""
