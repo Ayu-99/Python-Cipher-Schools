96cb5135c38bef7672426333bb9178013c47df62
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-11 09:23:52 +0530

    Create practice probs3.py

---
+++ KOC24 - INT108/ClassesNObjects/practice probs3.py
@@ -0,0 +1,4 @@
+"""
+1. take a number as input from user in form of a string, calculate sum of cube of individual digits of that number
+and then check whether the sum is equal to number itself or not.
+"""
