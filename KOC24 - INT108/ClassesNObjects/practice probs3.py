e76137885a4b6ae625a4a7257959e6b0f900a098
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-11 09:42:25 +0530

    Update practice probs3.py

--- KOC24 - INT108/ClassesNObjects/practice probs3.py
+++ KOC24 - INT108/ClassesNObjects/practice probs3.py
@@ -4,3 +4,22 @@ and then check whether the sum is equal to number itself or not.
 
 2. take an integer input from user and print how many digits are there in that number.
 """
+
+#1
+n = input("Enter a number")
+# "153" --->  "1" --> 1
+sum = 0
+for i in n:
+    sum += int(i)**3
+
+if sum ==  int(n):
+    print("it is armstrong number")
+else:
+    print("its not armstrong number")
+
+    
+#2
+
+    
+    
+    
