40cf577cf6bdf566e2c30cd8117db97a335b00d4
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-11 09:35:54 +0530

    Update practice probs3.py

--- KOC24 - INT108/ClassesNObjects/practice probs3.py
+++ KOC24 - INT108/ClassesNObjects/practice probs3.py
@@ -1,4 +1,6 @@
 """
 1. take a number as input from user in form of a string, calculate sum of cube of individual digits of that number
 and then check whether the sum is equal to number itself or not.
+
+2. take an integer input from user and print how many digits are there in that number.
 """
