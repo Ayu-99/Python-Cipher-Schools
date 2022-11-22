e22007b44284bb60e2c114d5b07d45ff0b4de821
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-22 09:14:00 +0530

    Create join function.py

---
+++ KOC07 - INT108/join function.py
@@ -0,0 +1,13 @@
+# s="this class*students*are way too boring"
+# l=list(s.split())
+# print(l)
+
+"""
+Join() function returns a string concatenated with the elements of iterable.
+
+"""
+
+l=["this", "class", "students", "are", "way", "tooo", "boring"]
+
+s = "$".join(l)
+print(s)
