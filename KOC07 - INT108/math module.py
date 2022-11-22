705317e0457ae8f97299398e3c2ce2dd1e2fc4db
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-22 08:39:05 +0530

    Create math module.py

---
+++ KOC07 - INT108/math module.py
@@ -0,0 +1,57 @@
+"""
+Python provides math module to deal with basic calculations like basic operations- addition, subtraction, multiplication
+and division and advance operations like trignometic, logarthmic and exponential functions.
+
+constants -whose value does not change
+pi - 3.14
+euler's number
+tau
+infinity -- int
+"""
+
+import math
+# from math import floor,ceil
+# print(math.e)
+# print(math.pi)
+#area of circle  pi * r * r
+# r = int(input("Enter the radius of circle: "))
+# area  = math.pi * r * r
+# print(area)
+# print(math.tau)
+# print(-math.inf)
+# print(math.nan)
+
+
+"""
+Numeric functions
+1. finding the ceil and floor of a number
+2. factorial of a number
+3. gcd(greatest common divisor) of a number
+4. square root of a number
+5. power of a number
+"""
+
+# 3.14 --> floor - 3
+# ceil - 4
+
+# x=3.14
+# print(floor(x))
+# # print(math.ceil(x))
+#
+
+# d= -45
+# y=abs(d)
+# print(y)
+
+# a=5
+# print(math.factorial(5))
+
+# a=15
+# b=5
+# print(math.gcd(a,b))
+
+# a=-10
+# print(math.fabs(a))
+
+print(math.pow(2,5))
+print(math.sqrt(64))
