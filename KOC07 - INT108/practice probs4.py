e7ee4b018f68bf6b6c40ee885775c1b2d51041e7
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-23 08:28:30 +0530

    Create practice probs4.py

---
+++ KOC07 - INT108/practice probs4.py
@@ -0,0 +1,43 @@
+"""
+#1
+You will be given multiple substrings(4), and you have to concatenate them using $
+
+i/p
+"hi" "hello" "how u doing" "nice weather"
+
+o/p
+"hi$hello$how u doing$nice weather"
+
+
+#2
+create a python class Birthday that takes two variables-
+1. a string called name
+2. a string called age
+
+do not forget init function
+
+create a member function that increases the value of age by 1
+create an instance/object of class and age should be 23 and name should be "Jack" for
+that object. call the age method to increase age of Jack and then print the age of
+Jack.
+
+#3
+you have a list of characters. you have to print count of each character in list
+take character list input from user
+
+i/p
+7
+a
+b
+c
+a
+b
+c
+c
+
+o/p
+a : 2
+b : 2
+c : 3
+
+"""
