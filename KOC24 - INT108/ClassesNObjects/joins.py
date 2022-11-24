ca8aa45dc1dd2eb0cb8a7440379a236e09179096
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-24 08:56:27 +0530

    Create joins.py

---
+++ KOC24 - INT108/ClassesNObjects/joins.py
@@ -0,0 +1,12 @@
+# s="this classstudents*are way too*boring"
+# l=list(s.split("*"))
+# print(l)
+
+"""
+Join() function returns a string concatenated with the elements of iterable.
+string-sub strings --> split
+join-- sub strings  ---> string
+"""
+l=['this classstudents', 'are way too', 'boring']
+l1 = '$'.join(l)
+print(l1)
