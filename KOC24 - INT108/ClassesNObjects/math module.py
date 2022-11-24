b77b982334fa3c901ff988e9de8d70d25fd683b9
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-24 08:19:28 +0530

    Create math module.py

---
+++ KOC24 - INT108/ClassesNObjects/math module.py
@@ -0,0 +1,25 @@
+"""
+python math module provides functions/methods to deal with basic operations such as addition, subtraction,
+multiplication, division and advance operations like trignometric, logarthmic and exponential functions.
+
+
+Constants -
+1. pi 3.14
+2. euler's number
+3. tau
+4. infinity
+
+"""
+
+import math
+
+# print(math.pi)
+# print(math.e)
+# print(math.tau)
+# print(-math.inf)
+
+#area of circle
+r = int(input("Enter the radius of circle:"))
+area = math.pi * r * r
+print("area of circle is: ", area)
+
