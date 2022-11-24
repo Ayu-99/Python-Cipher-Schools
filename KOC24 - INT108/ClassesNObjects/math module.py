c7943875a12ab6f400d1eaf429e41edb7fba6932
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-24 08:41:10 +0530

    Update math module.py

--- KOC24 - INT108/ClassesNObjects/math module.py
+++ KOC24 - INT108/ClassesNObjects/math module.py
@@ -9,9 +9,19 @@ Constants -
 3. tau
 4. infinity
 
+functions:
+1. ceil
+2. floor
+3. pow
+4. sqrt
+5. fabs
+6. gcd
+7. fact
+
 """
 
 import math
+# from math import ceil, floor
 
 # print(math.pi)
 # print(math.e)
@@ -19,7 +29,28 @@ import math
 # print(-math.inf)
 
 #area of circle
-r = int(input("Enter the radius of circle:"))
-area = math.pi * r * r
-print("area of circle is: ", area)
+# r = int(input("Enter the radius of circle:"))
+# area = math.pi * r * r
+# print("area of circle is: ", area)
+
+# x=3.14
+# print(ceil(x))
+# print(floor(x))
+
+# y = math.pow(2, 3)
+# print(y)
+#
+# z = math.sqrt(64)
+# print(z)
+
+# y= -4.5
+# print(math.fabs(y))
+
+# x=5
+# print(math.factorial(5))
+
+x=45
+y=15
+print(math.gcd(x,y))
+
 
