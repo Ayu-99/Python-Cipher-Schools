5a266f1398b2e69b8149fc9c671f69fbd4fe2892
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-26 13:00:20 +0530

    Update practice probs5.py

--- KOC24 - INT108/ClassesNObjects/practice probs5.py
+++ KOC24 - INT108/ClassesNObjects/practice probs5.py
@@ -9,6 +9,7 @@ Expected Output : 'world .py hello'
 4. Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle.
 """
 
+#1
 class ReverseClass:
     def reverseTheString(self, s):
         l = list(s.split(" "))
@@ -19,3 +20,44 @@ class ReverseClass:
 r = ReverseClass()
 finalString = r.reverseTheString("hello .py world")
 print(finalString)
+
+
+#2
+class get_String:
+    def get_string(self):
+        self.str1=input()
+    def print_string(self):
+        print(self.str1.upper())
+str1=get_String()
+str1.get_string()
+str1.print_string()
+
+
+#3
+class Rectangle:
+    def Area(self,a,b):
+        return a*b
+    def Perimeter(self,a,b):
+        return 2*(a+b)
+a=2
+b=3
+r = Rectangle()
+A=r.Area(a,b)
+P=r.Perimeter(a,b)
+print(A)
+print(P)
+
+
+#4
+import math
+class Circle:
+    def Area(self,rad):
+        return math.pi*rad*rad
+    def Perimeter(self,rad):
+        return 2*math.pi*rad
+rad=int(input())
+r = Circle()
+A=r.Area(rad)
+P=r.Perimeter(rad)
+print(A)
+print(P)
