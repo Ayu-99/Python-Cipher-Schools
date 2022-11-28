2a4951f236a905019abcf4b457cd97c32a086227
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-28 09:36:13 +0530

    Update practice probs6.py

--- KOC07 - INT108/practice probs6.py
+++ KOC07 - INT108/practice probs6.py
@@ -15,3 +15,12 @@ o/p=
 [0,1]
 
 """
+def func(x,t):
+    for i in range(len(x)):
+        for j in range(i+1,len(x)):
+            if x[i] + x[j] == t:
+                return i,j
+
+l = [2,4,6,8]
+r = 10
+print(list(func(l,r)))
