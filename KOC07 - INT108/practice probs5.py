93f9f33a055cf5a72030f1507fcfed385bc1132a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-28 08:11:39 +0530

    Create practice probs5.py

---
+++ KOC07 - INT108/practice probs5.py
@@ -0,0 +1,10 @@
+"""
+1. Write a Python class to reverse a string word by word.
+Input string : 'hello .py world'
+Expected Output : 'world .py hello'
+
+2. Write a Python class which has two methods/functions get_String and print_String. get_String accept a string from the user and print_String print the string in upper case.
+3. Write a Python class named Rectangle constructed by a length and width and a method which will compute the area of a rectangle.
+
+4. Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle.
+"""
