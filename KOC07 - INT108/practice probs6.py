f875039146c3bfade22af3262d8e8a0fd2de6cb3
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-28 09:00:33 +0530

    Create practice probs6.py

---
+++ KOC07 - INT108/practice probs6.py
@@ -0,0 +1,17 @@
+"""
+1. given a list of numbers nums and an integer target, return the indices of two
+numbers such that they add up to the target.
+
+you may assume that each input would have exactly one solution, and you may not use the
+same element twice.
+
+you can return the answer in any order.
+
+i/p
+nums=[2,7,11,15]
+target = 9
+
+o/p=
+[0,1]
+
+"""
