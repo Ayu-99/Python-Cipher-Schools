2fb8698f3a71c9343f48c52d411183980fcf9d8d
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-11-30 09:57:07 +0530

    Update practice probs7.py

--- KOC24 - INT108/ClassesNObjects/practice probs7.py
+++ KOC24 - INT108/ClassesNObjects/practice probs7.py
@@ -18,6 +18,22 @@ Output: [1,2]
 Explanation: After sorting, nums is [1,2,2,3,5].
 The indices where nums[i] == 2 are 1 and 2.
 
+#3
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given a list of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+
+Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+Output: 6
+Explanation:
+- The first sentence, "alice and bob love leetcode", has 5 words in total.
+- The second sentence, "i think so too", has 4 words in total.
+- The third sentence, "this is great thanks very much", has 6 words in total.
+Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+
+
 """
 
 #1
@@ -42,3 +58,6 @@ for i in range(len(l)):
         output.append(i)
 
 print(output)
+
+
+
