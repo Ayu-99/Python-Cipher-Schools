a2eb434f777ccf7661e3f97af508c48da30dd77a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-01 08:08:14 +0530

    Update practice probs7.py

--- KOC24 - INT108/ClassesNObjects/practice probs7.py
+++ KOC24 - INT108/ClassesNObjects/practice probs7.py
@@ -33,6 +33,13 @@ Explanation:
 - The third sentence, "this is great thanks very much", has 6 words in total.
 Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
 
+#4
+Given a list of integers arr, return true if the number of occurrences of each value in the array is unique, or false otherwise.
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+
 
 """
 
