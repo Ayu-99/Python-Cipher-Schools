c72c7999a850260f800a0ec5e850274b87c8c53a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-05 08:08:28 +0530

    Create practice probs8.py

---
+++ KOC07 - INT108/practice probs8.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+https://leetcode.com/problems/squares-of-a-sorted-array/
+
+https://leetcode.com/problems/sorting-the-sentence/
+
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+https://leetcode.com/problems/determine-if-string-halves-are-alike/
+"""
+
