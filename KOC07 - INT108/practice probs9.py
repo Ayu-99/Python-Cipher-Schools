667f84c4ec54dfff88e4fc9282f21c5fc522f8db
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-07 07:58:33 +0530

    Update practice probs9.py

--- KOC07 - INT108/practice probs9.py
+++ KOC07 - INT108/practice probs9.py
@@ -3,3 +3,17 @@ https://leetcode.com/problems/sort-the-people/
 
 https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence/
 """
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        l = []
+        for i in range(len(names)):
+            l.append([heights[i], names[i]])
+            
+        l.sort(reverse=True)
+        output=[]
+        for i in l:
+            output.append(i[1])
+        return output   
+            
+        
