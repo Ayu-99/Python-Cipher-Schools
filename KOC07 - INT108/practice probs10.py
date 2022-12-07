f1a170d4aed188c972c9137de4b6a2bb8a632afb
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-07 08:47:52 +0530

    Update practice probs10.py

--- KOC07 - INT108/practice probs10.py
+++ KOC07 - INT108/practice probs10.py
@@ -4,3 +4,25 @@ https://leetcode.com/problems/count-the-number-of-consistent-strings/
 https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
 """
 
+#1
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        d={}
+        for i in range(len(allowed)):
+            d[allowed[i]] = 1
+            
+        count = 0
+        
+        for word in words:
+            consistent=True
+            for ch in word:
+                if ch not in d:
+                    consistent=False
+            
+            if consistent == True:
+                count += 1
+                    
+        
+        return count
+        
+        
