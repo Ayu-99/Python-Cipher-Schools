7946965025a9fbebd4b527d18232ecb7865b32e7
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-17 12:29:18 +0530

    Create Exception Handling.py

---
+++ KOC24 - INT108/ClassesNObjects/Exception Handling.py
@@ -0,0 +1,34 @@
+"""
+Exception Handling -- v.v.v imp
+try block
+exception handling
+
+there are two types of errors:
+1. compile time error:  read about compile time error
+2. run time error: divisionByZero, value error, key error
+
+exception - run time error
+"""
+
+
+"""
+data error
+value error
+syantax error
+key error
+ZeroDivisionError
+"""
+
+
+try :
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    c=int(input())
+    print(a//c)
+
+except:
+    print("error occurred")
+
+
+
