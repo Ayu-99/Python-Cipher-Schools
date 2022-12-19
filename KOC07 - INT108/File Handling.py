ee55efc812e3327150f597dcd595213cc3164edc
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-19 09:13:46 +0530

    Create File Handling.py

---
+++ KOC07 - INT108/File Handling.py
@@ -0,0 +1,24 @@
+"""
+Python supports file handling  i.e we can read and write files along with many other file
+handling operations to operate on files.
+
+
+Before performing any operation on the file like reading or writing, first we have to
+open that file. For this, we can use inbuild function open()
+
+Modes -
+1. r : open an existing file for a read operation
+2. w : open an existing file for a write operation, If the file has alreaady some content,
+it will be overriden, but if the file is not created, then it will create a new file.
+3. a : open an existing file for append operation. It will not override data.
+4. r+  : To read and write data into file. Previous data will be overriden
+5. w+ : To write and then read data. Previous data will be overriden.
+6. a+ : To append and read data from file. It won't override existing data.
+
+"""
+
+# file = open('text1.txt', 'r')
+# print(file.read())
+
+file = open('text1.txt', 'r')
+print(file.read(5))
