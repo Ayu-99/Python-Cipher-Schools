c60d4c88bae13ef93c08187d4eff232d975f7e6c
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-26 08:11:50 +0530

    Create practice probs13.py

---
+++ KOC07 - INT108/practice probs13.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/percentage-of-letter-in-string/
+
+https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/
+
+***********************************
+
+https://leetcode.com/problems/valid-anagram/
+
+https://leetcode.com/problems/most-frequent-even-element/
+
+"""
