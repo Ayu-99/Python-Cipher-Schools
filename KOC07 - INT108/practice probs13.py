730fd0c88f1e672e0bf01e952056463596528eab
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-26 09:35:28 +0530

    Update practice probs13.py

--- KOC07 - INT108/practice probs13.py
+++ KOC07 - INT108/practice probs13.py
@@ -35,6 +35,13 @@ class Solution:
       
       
 #3
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1=list(s)
+        l2=list(t)
+        l1.sort()
+        l2.sort()
+        return l1==l2
+        
 
 #4
