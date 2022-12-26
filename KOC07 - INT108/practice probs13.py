6e967586b52137babf91fdb7823f915bb388ba5f
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-26 09:32:53 +0530

    Update practice probs13.py

--- KOC07 - INT108/practice probs13.py
+++ KOC07 - INT108/practice probs13.py
@@ -10,3 +10,31 @@ https://leetcode.com/problems/valid-anagram/
 https://leetcode.com/problems/most-frequent-even-element/
 
 """
+
+#1
+class Solution:
+    def percentageLetter(self, a: str, b: str) -> int:
+        count=0
+        for i in range(len(a)):
+            if a[i]==b:
+                count+=1
+        zz=count/len(a)
+        zzz=zz*100
+        return (int(zzz))
+
+#2
+class Solution:
+    def digitCount(self, num: str) -> bool:
+        for i in range(len(num)):
+
+            if int(num.count(str(i)))!=int(num[i]):
+
+                return False
+                
+        return True
+      
+      
+#3
+
+
+#4
