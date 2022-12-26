28e9105289ddb536a8de9fdf65cb37720796d3ea
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-26 09:47:09 +0530

    Update practice probs13.py

--- KOC07 - INT108/practice probs13.py
+++ KOC07 - INT108/practice probs13.py
@@ -45,3 +45,26 @@ class Solution:
         
 
 #4
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                if nums[i] in d:
+                    d[nums[i]] += 1
+                else:
+                    d[nums[i]] = 1
+                    
+        l = list(d.keys())
+        l.sort()
+        
+        answer = -1
+        maxCount = 0
+        for i in l:
+            maxCount = max(maxCount, d[i])
+        
+        for i in l:
+            if d[i] == maxCount:
+                return i
+        
+        return answer
