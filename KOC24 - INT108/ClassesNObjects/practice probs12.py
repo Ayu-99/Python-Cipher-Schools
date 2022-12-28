77dbacd8b89e8691492d3caf08c10ec787617466
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-28 09:42:43 +0530

    Update practice probs12.py

--- KOC24 - INT108/ClassesNObjects/practice probs12.py
+++ KOC24 - INT108/ClassesNObjects/practice probs12.py
@@ -3,3 +3,18 @@ https://leetcode.com/problems/first-letter-to-appear-twice/
 
 https://leetcode.com/problems/number-of-distinct-averages/
 """
+
+#1
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        l=[]
+        for i in s:
+            if i in l:
+                return i
+            else:
+                l.append(i)
+                
+        
+        
+ #2
+
