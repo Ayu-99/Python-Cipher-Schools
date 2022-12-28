b042c72a7a12f013bb54148c20c8347eedbdf231
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-28 08:48:24 +0530

    Update practice probs14.py

--- KOC07 - INT108/practice probs14.py
+++ KOC07 - INT108/practice probs14.py
@@ -5,6 +5,22 @@ https://leetcode.com/problems/first-letter-to-appear-twice/
 """
 
 #1
+class Solution:
+    def distinctAverages(self, nums: List[int]) -> int:
+        nums.sort()
+        s=set()
+        i=0
+        j=len(nums)-1
+        
+        while i < j:
+            mine = nums[i]
+            maxe = nums[j]
+            avg = (maxe+mine)/2
+            s.add(avg)
+            i += 1
+            j -= 1
+            
+        return len(s)
 
 
 #2
